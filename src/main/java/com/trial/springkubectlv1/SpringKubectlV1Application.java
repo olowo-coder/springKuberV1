dd21bc5c557cf521a5b8ac616c9f95e3ce1036c0
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-05-08 13:52:06 +0100

    set up

---
+++ src/main/java/com/trial/springkubectlv1/SpringKubectlV1Application.java
@@ -0,0 +1,13 @@
+package com.trial.springkubectlv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubectlV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKubectlV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/trial/springkubectlv1/SpringKubectlV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trial.springkubectlv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKubectlV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
